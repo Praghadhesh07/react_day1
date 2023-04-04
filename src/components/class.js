27b77970fc53dcf7cd54d3cbff4942677002d5aa
Praghadhesh-7 <727721euec111@skcet.ac.in>
2023-04-04 21:05:49 +0530

    First commit

---
+++ src/components/class.js
@@ -0,0 +1,7 @@
+import React from "react";
+class MyClass extends React.Component{
+    render(){
+        return <h2>Welcome</h2>
+    }
+}
+export default MyClass;
---
+++ src/components/comoponent.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Test()
+{
+   return (
+    <button onClick={window['array_function']}>Click</button>
+   );
+}
+export default Test;
